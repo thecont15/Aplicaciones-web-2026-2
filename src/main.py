fa606ebfd7d52f77d75ba8755aadaaca320c855c
thecont15 <thecont815@gmail.com>
2026-08-31 20:23:08 -0500

    feat:Agregar solo vendedor

--- src/main.py
+++ src/main.py
@@ -2,13 +2,8 @@ from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
 
-from src.api.cliente import router as cliente_router
-from src.api.inventario import router as inventario_router
 from src.api.vendedor import router as vendedor_router
 from src.database.database import Base, engine
-from src.entities import cliente as _cliente_model
-from src.entities import inventario as _inventario_model
-from src.entities import vendedor as _vendedor_model
 
 
 @asynccontextmanager
@@ -19,8 +14,6 @@ async def lifespan(app: FastAPI):
 
 app = FastAPI(title="API de Vendedores", lifespan=lifespan)
 app.include_router(vendedor_router)
-app.include_router(cliente_router)
-app.include_router(inventario_router)
 
 
 @app.get("/")
