64bed86359e3bde2d4649b1c4f2c1980206d4757
Emmanuel Cardenas <emmanuelcardenas327087@correo.itm.edu.co>
2026-09-17 22:22:08 -0500

    feat: integracion CI con Pylint, CD de  Emmanuel y entidad Cliente con seeders

--- src/main.py
+++ src/main.py
@@ -3,8 +3,10 @@ from contextlib import asynccontextmanager
 from fastapi import FastAPI
 
 from src.database.database import engine
+from src.entities.cliente import Cliente
 from src.entities.inventario import Inventario
 from src.entities.vendedor import Vendedor
+from src.routers.cliente import router as cliente_router
 from src.routers.inventario import router as inventario_router
 from src.routers.vendedor import router as vendedor_router
 
@@ -13,14 +15,16 @@ from src.routers.vendedor import router as vendedor_router
 async def lifespan(app: FastAPI):
     Vendedor.metadata.create_all(bind=engine)
     Inventario.metadata.create_all(bind=engine)
+    Cliente.metadata.create_all(bind=engine)
     yield
 
 
-app = FastAPI(title="API de Vendedores", lifespan=lifespan)
+app = FastAPI(title="API de la Clase", lifespan=lifespan)
 app.include_router(vendedor_router)
 app.include_router(inventario_router)
+app.include_router(cliente_router)
 
 
 @app.get("/")
 def inicio():
-    return {"mensaje": "API de vendedores activa"}
+    return {"mensaje": "API de la Clase activa"}
